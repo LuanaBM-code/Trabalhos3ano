a38cbdcbdde6e5ee6fa0177d3bf01b8e0c512216
Luana Bruch Maurer <104578302+LuanaBM-code@users.noreply.github.com>
2023-01-25 17:50:40 -0300

    Update cadastrarlivro.php

--- BibliotecaVirtual/cadastrarlivro.php
+++ BibliotecaVirtual/cadastrarlivro.php
@@ -19,7 +19,7 @@ if(isset($_SESSION['idioma'])){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <title><?=$texto['livros']?>.</title>
+    <title><?=$texto['livros']?></title>
 </head>
 <body>
   
