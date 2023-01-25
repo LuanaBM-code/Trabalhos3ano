f02890b91c20977d0db2011cf93443b42f32cb3d
Luana Bruch Maurer <104578302+LuanaBM-code@users.noreply.github.com>
2023-01-25 17:58:32 -0300

    Update editarlivro.php

--- BibliotecaVirtual/editarlivro.php
+++ BibliotecaVirtual/editarlivro.php
@@ -20,7 +20,7 @@ if(isset($_SESSION['idioma'])){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <title><?=$texto['editarli']?></title>
+    <title><?=$texto['editarr']?></title>
 </head>
 <body>
   
